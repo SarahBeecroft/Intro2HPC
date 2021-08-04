5148a257c1c199ed2c6bc2202684ceadcd5b9e8d
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2021-08-04 18:03:36 +0800

    Update blastarray.sh

--- exercises/jobarray/blastarray.sh
+++ exercises/jobarray/blastarray.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -l
+
+# SLURM directives
+#
+# This is an array job with four subtasks 
+
 #SBATCH --reservation=UWAHPC
 #SBATCH -n 1 
-#SBATCH --array=1-4
+#SBATCH --array=0-3
 #SBATCH -A courses01
 #SBATCH -t 00:05:00
 #SBATCH --export=NONE
+ 
+ 
+echo This job shares a SLURM array job ID with the parent job: $SLURM_ARRAY_JOB_ID
+echo This job has a SLURM job ID: $SLURM_JOBID
+echo This job has a unique SLURM array index: $SLURM_ARRAY_TASK_ID
+
 
 # Load Blast+ module
 module load blast+
