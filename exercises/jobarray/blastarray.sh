885e12f637fbb12e7cda92f8459495113b2f85ca
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2021-08-06 16:18:10 +0800

    Update blastarray.sh

--- exercises/jobarray/blastarray.sh
+++ exercises/jobarray/blastarray.sh
@@ -3,19 +3,29 @@
 # SLURM directives
 #
 # This is an array job with four subtasks 
-
 #SBATCH --reservation=UWAHPC
-#SBATCH -n 1 
-#SBATCH --array=0-3
-#SBATCH -A courses01
-#SBATCH -t 00:05:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-4
+#SBATCH --account=courses01
+#SBATCH --time=00:05:00
 #SBATCH --export=NONE
+#SBATCH --output=MYJOB-%j.log
  
- 
-echo This job shares a SLURM array job ID with the parent job: $SLURM_ARRAY_JOB_ID
-echo This job has a SLURM job ID: $SLURM_JOBID
-echo This job has a unique SLURM array index: $SLURM_ARRAY_TASK_ID
 
+#Do not edit the echo sections
+echo "All jobs in this array have:"
+echo "- SLURM_ARRAY_JOB_ID=${SLURM_ARRAY_JOB_ID}"
+echo "- SLURM_ARRAY_TASK_COUNT=${SLURM_ARRAY_TASK_COUNT}"
+echo "- SLURM_ARRAY_TASK_MIN=${SLURM_ARRAY_TASK_MIN}"
+echo "- SLURM_ARRAY_TASK_MAX=${SLURM_ARRAY_TASK_MAX}"
+echo "This job in the array has:"
+echo "- SLURM_JOB_ID=${SLURM_JOB_ID}"
+echo "- SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID}"
+
+
+# Swap gcc module version
+module swap gcc/4.8.5 gcc/8.3.0
 
 # Load Blast+ module
 module load blast+
