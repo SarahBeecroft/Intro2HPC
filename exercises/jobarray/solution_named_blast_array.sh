227da62b217460e55d1717cf5f39298b244e8835
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2021-08-06 17:35:37 +0800

    Update and rename named_blast_array.sh to solution_named_blast_array.sh

--- exercises/jobarray/solution_named_blast_array.sh
+++ exercises/jobarray/solution_named_blast_array.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
+#!/bin/bash -l
+
+# SLURM directives
+#
+# This is an array job with four subtasks 
+#SBATCH --reservation=UWAHPC
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=1:00:00
-#SBATCH --job-name=MYJOB
-#SBATCH --partition=workq
-#SBATCH --account=MYACCOUNT
+#SBATCH --array=1-4
+#SBATCH --account=courses01
+#SBATCH --time=00:05:00
+#SBATCH --export=NONE
 #SBATCH --output=MYJOB-%j.log
-#SBATCH --error=MYJOB-%J.log
+ 
  
 #Do not edit the echo sections
 echo "All jobs in this array have:"
@@ -20,11 +25,17 @@ echo "- SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID}"
  
  
 # alter the following line to suit your files. It will grab all files matching whatever regular expression you provide.
-FILES=($(ls -1 *.bam))
+FILES=($(ls -1 *.fasta))
  
 # grabs our filename from a directory listing
 FILENAME=${FILES[$SLURM_ARRAY_TASK_ID]}
 echo "My input file is ${FILENAME}"
- 
-#load modules
+
+# Swap gcc module version
+module swap gcc/4.8.5 gcc/8.3.0
+
+# Load Blast+ module
 module load blast+
+
+# Run Blast comparisons
+blastp -use_sw_tback -query ${FILENAME} -db zebrafish.1.protein.faa -out result.${FILENAME}.txt
