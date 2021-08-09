be871451fe6df7adf5df51ed89ebb9be38f31f1a
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2021-08-09 13:12:44 +0800

    Update download.sh

--- exercises/jobarray/download.sh
+++ exercises/jobarray/download.sh
@@ -6,9 +6,9 @@
 #SBATCH -t 00:05:00
 #SBATCH --export=NONE
 
-wget https://www.uniprot.org/uniprot/Q4LEZ3.fasta -O 1.fasta
-wget https://www.uniprot.org/uniprot/Q96SE0.fasta -O 2.fasta
-wget https://www.uniprot.org/uniprot/Q9UGJ0.fasta -O 3.fasta
-wget https://www.uniprot.org/uniprot/Q9H221.fasta -O 4.fasta
+wget https://www.uniprot.org/uniprot/Q4LEZ3.fasta -O zebrafish_Q4LEZ3.fasta
+wget https://www.uniprot.org/uniprot/Q96SE0.fasta -O zebrafish_Q96SE0.fasta
+wget https://www.uniprot.org/uniprot/Q9UGJ0.fasta -O zebrafish_Q9UGJ0.fasta
+wget https://www.uniprot.org/uniprot/Q9H221.fasta -O zebrafish_Q9H221.fasta
 curl -O ftp://ftp.ncbi.nih.gov/refseq/D_rerio/mRNA_Prot/zebrafish.1.protein.faa.gz
 gunzip -f zebrafish.1.protein.faa.gz
