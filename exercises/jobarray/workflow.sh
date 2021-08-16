2a8c2cf8cbfc74483e945b1b598cb9a456ad1c1f
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2021-08-16 14:54:45 +0800

    Delete workflow.sh

--- exercises/jobarray/workflow.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DOWNLOAD=$(sbatch download.sl | cut -f 4 -d' ')
-echo $DOWNLOAD submitted
-MAKEDB=$(sbatch -d afterok:$DOWNLOAD makedb.sl | cut -f 4 -d' ')
-echo $MAKEDB submitted
-BLASTARRAY=$(sbatch -d afterok:$MAKEDB blastarray.sl | cut -f 4 -d' ')
-echo $BLASTARRAY submitted
