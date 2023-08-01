24bc147dc5833914172c26548547819aaf7e7211
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2023-08-01 19:03:30 +0800

    Update makedb.sh

--- exercises/jobarray/makedb.sh
+++ exercises/jobarray/makedb.sh
@@ -1,16 +1,12 @@
 #!/bin/bash -l
-#SBATCH --reservation=UWAHPC
+#SBATCH --reservation=UWA
 #SBATCH -A courses01
 #SBATCH -n 1 
 #SBATCH -t 00:05:00
 #SBATCH --export=NONE
 
-# Swap gcc module version
-module swap gcc/4.8.5 gcc/8.3.0
-
 # Load Blast+ module
-module load blast+
-
+module load blast/2.12.0--pl5262h3289130_0
 bash download.sh
 
 # Make Blast DB
