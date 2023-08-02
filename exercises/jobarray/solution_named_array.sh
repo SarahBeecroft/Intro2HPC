9ebef6b9f64a028784b4bd1589391f7251dc92f4
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2023-08-02 19:03:57 +0800

    Update solution_named_array.sh

--- exercises/jobarray/solution_named_array.sh
+++ exercises/jobarray/solution_named_array.sh
@@ -3,7 +3,7 @@
 # SLURM directives
 #
 # This is an array job with four subtasks 
-#SBATCH --reservation=UWAHPC
+#SBATCH --reservation=UWATraining
 #SBATCH --account=courses01
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
